2fc8397358f2d040de6517f89b8b32ebc3bbf609
Washim Akram <90306771+AlienWashim@users.noreply.github.com>
2025-02-24 21:46:25 +0600

    Longest Consecutive Sequence - LeetCode Medium

---
+++ Longest Consecutive Sequence - LeetCode Medium.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        if (nums.Length == 0) return 0;
+
+        Array.Sort(nums);
+
+        int longest = 1;
+        int currentStreak = 1;
+
+        for(int i = 1; i < nums.Length; i++){
+            if(nums[i] == nums[i - 1] + 1){
+                currentStreak++;
+            }
+            else if(nums[i] != nums[i - 1]){
+                longest = Math.Max(longest, currentStreak);;
+                currentStreak = 1;
+            }
+        }
+
+        return Math.Max(longest, currentStreak);
+    }
+}
