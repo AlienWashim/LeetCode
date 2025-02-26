57bd4e56eae1a7989150d295cd3a62b0937437bd
Washim Akram <90306771+AlienWashim@users.noreply.github.com>
2025-02-26 11:24:11 +0600

    Minimum Size Subarray Sum - LeetCode Medium

---
+++ Minimum Size Subarray Sum - LeetCode Medium.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MinSubArrayLen(int target, int[] nums) {
+        int left = 0, sum = 0, minLength = int.MaxValue;
+        for (int right = 0; right < nums.Length; right++) {
+            sum += nums[right];
+            while (sum >= target) {
+                minLength = Math.Min(minLength, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+        return minLength == int.MaxValue ? 0 : minLength;
+    }
+}
